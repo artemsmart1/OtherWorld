2680b9d5742745dc512153edf7bad2cd15e83412
Berserker <4poker.55@gmaill.com>
2020-06-08 23:03:31 +0700

    2 lesson half-done

--- app/src/main/java/com/example/otherworld/HomeActivity.java
+++ app/src/main/java/com/example/otherworld/HomeActivity.java
@@ -1,14 +1,92 @@
 package com.example.otherworld;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.otherworld.Common.Common;
+import com.example.otherworld.Fragments.GamesFragment;
+import com.example.otherworld.Fragments.HomeFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Collection;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
+
+    BottomSheetDialog bottomSheetDialog;
+
+    CollectionReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(HomeActivity.this);
+
+        //Init
+        userRef = FirebaseFirestore.getInstance().collection("User");
+
+        //Проверка сети
+        if(getIntent() != null){
+            boolean isLogin = getIntent().getBooleanExtra(Common.IS_LOGIN,false);
+            if(isLogin){
+                //проверяем существование юзера
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                DocumentReference currentUser = userRef.document(user.getPhoneNumber());
+                currentUser.get()
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()){
+                                DocumentSnapshot userSnapShot = task.getResult();
+                                if(!userSnapShot.exists()){
+                                    showUpdateDialog(user.getPhoneNumber());
+                                }
+
+                            }
+                        });
+             }
+        }
+
+        //Вид
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.action_home)
+                    fragment = new HomeFragment();
+                else if(menuItem.getItemId() == R.id.action_playing)
+                    fragment = new GamesFragment();
+                return loadFragment(fragment);
+            }
+        });
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_contain,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    private void showUpdateDialog(String phoneNumber) {
     }
 }
