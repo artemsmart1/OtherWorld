bff17c5ebd880ead05f8ca5040885c064729ffb5
Berserker <4poker.55@gmaill.com>
2020-06-08 17:49:14 +0700

    1 lesson(Fireauth,lesson 32)

--- app/src/main/java/com/example/otherworld/MainActivity.java
+++ app/src/main/java/com/example/otherworld/MainActivity.java
@@ -1,34 +1,116 @@
 package com.example.otherworld;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.accounts.Account;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.otherworld.Common.Common;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.provider.FirebaseInitProvider;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int APP_REQEST_CODE=1488;
 
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+
     @BindView(R.id.btn_login)
     Button btn_login;
     @BindView(R.id.txt_skip)
     TextView txt_skip;
 
-    @OnClick({R.id.btn_login)
- //   void login_user(){
+    @OnClick(R.id.btn_login)
+    void login_user()
+    {
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),APP_REQEST_CODE);
 
-      //  final intent intent = new intent (this, Account)
-    //}
-}
+    }
+
+    @OnClick(R.id.txt_skip)
+    void skipLogin(){
+        Intent intent = new Intent(this,HomeActivity.class);
+        intent.putExtra(Common.IS_LOGIN,false);
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        if(authStateListener != null)
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == APP_REQEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK)
+            {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }
+            else
+                {
+                    Toast.makeText(this,"Не удалось войти",Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null)//уже залогинин
+        {
+            Intent intent = new Intent(this,HomeActivity.class);
+            intent.putExtra(Common.IS_LOGIN,true);
+            startActivity(intent);
+            finish();
+        }
+        else
+            {
+                setContentView(R.layout.activity_main);
+                ButterKnife.bind(MainActivity.this);
+            }
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+    }
+
+    private void checkUserFromFirebase (FirebaseUser user)
+    {
+    //    FirebaseMessaging.getInstance().
     }
 }
