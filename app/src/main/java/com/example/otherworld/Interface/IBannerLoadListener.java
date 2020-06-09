689e22018c0f9a15332c6df1029efcc5f041b5f9
Berserker <4poker.55@gmaill.com>
2020-06-09 21:40:16 +0700

    before androidX

---
+++ app/src/main/java/com/example/otherworld/Interface/IBannerLoadListener.java
@@ -0,0 +1,10 @@
+package com.example.otherworld.Interface;
+
+import com.example.otherworld.Model.Banner;
+
+import java.util.List;
+
+public interface IBannerLoadListener {
+    void onBannerLoadSuccess(List<Banner> banners);
+    void onBannerLoadFailed(String message);
+}
---
+++ app/src/main/java/com/example/otherworld/Model/Banner.java
@@ -0,0 +1,21 @@
+package com.example.otherworld.Model;
+
+public class Banner {
+    //Банер и ветрина картинок
+    private String image;
+
+    public Banner() {
+    }
+
+    public Banner(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
